24eaa0e95a0f0617e2fd267e67def55723e5d4f5
hao wang <ge_nah@hotmail.com>
2019-06-05 03:36:36 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,7 +1,8 @@
 require "pry"
 def count_elements(array)
   # code goes here
-  hash = Hash.new(0) 
+  hash = Hash.new(0)
+  binding.pry
   array.each {|word| hash[word] += 1}
   binding.pry
   hash
